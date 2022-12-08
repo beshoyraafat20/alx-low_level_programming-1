ce297e40a87afe2bc71ea24a33d472d4f9459edd
SalmaWR <Salma.wagdy@hotmail.com>
2022-12-09 00:11:50 +0200

    edite file.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
-include<stdio.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return : Always 0 (success)
+ */
 int main(void)
 {
 	int puts("Programming is like building a multilingual puzzle\n");
